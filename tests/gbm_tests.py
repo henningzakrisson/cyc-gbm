c32e0eac58b3af9fa4f00dda06f81c1167bd6c68
henningzakrisson <henning.zakrisson@gmail.com>
2023-04-15 15:09:20 +0200

    rename stuff

--- tests/gbm_tests.py
+++ tests/gbm_tests.py
@@ -1,14 +1,15 @@
 import unittest
 import numpy as np
-from src.UniGBM import UniGBM, tune_kappa
+from src.uni_gbm import UniGBM, tune_kappa
+from src.cyc_gbm import CycGBM
 
 
-class UniGBMTest(unittest.TestCase):
+class GBMTests(unittest.TestCase):
     """
-    A class that defines unit tests for the `UniGBM` class.
+    A class that defines unit tests for the `GBM` classes.
     """
 
-    def test_normal_distribution(self):
+    def test_normal_distribution_uni(self):
         """
         Test method for the `UniGBM` class on a dataset where the target variable
         follows a normal distribution.
@@ -37,10 +38,10 @@ class UniGBMTest(unittest.TestCase):
             first=loss,
             second=expected_loss,
             places=5,
-            msg="Normal distribution loss not as expected",
+            msg="UniGBM Normal distribution loss not as expected",
         )
 
-    def test_gamma_distribution(self):
+    def test_gamma_distribution_uni(self):
         """
         Test method for the `UniGBM` class on a dataset where the target variable
         follows a gamma distribution.
@@ -70,10 +71,10 @@ class UniGBMTest(unittest.TestCase):
             first=sse,
             second=expected_sse,
             places=5,
-            msg="Gamma distribution sse not as expected",
+            msg="UniGBM Gamma distribution sse not as expected",
         )
 
-    def test_kappa_tuning(self):
+    def test_kappa_tuning_uni(self):
         """Tests the `tune_kappa` function to ensure it returns the correct value of the kappa parameter."""
         expected_kappa = 35
         n = 1000
@@ -91,5 +92,40 @@ class UniGBMTest(unittest.TestCase):
         self.assertEqual(
             first=kappa,
             second=expected_kappa,
-            msg="Optimal number of boosting steps not correct",
+            msg="Optimal number of boosting steps not correct for UniGBM",
+        )
+
+    def test_normal_distribution_cyc(self):
+        """
+        Test method for the `CycGBM` class on a dataset where the target variable
+        follows a normal distribution.
+
+        :raises AssertionError: If the calculated loss does not match the expected loss
+            to within a tolerance.
+        """
+        n = 100
+        expected_loss = 641.9173857564037
+        rng = np.random.default_rng(seed=10)
+        X0 = np.arange(0, n)
+        X1 = np.arange(0, n)
+        rng.shuffle(X1)
+        mu = 10 * (X0 > 0.3 * n) + 5 * (X1 > 0.5 * n)
+        sigma = np.exp(1 + 1 * (X0 < 0.4 * n))
+
+        X = np.stack([X0, X1]).T
+        y = rng.normal(mu, sigma)
+
+        kappas = [100, 10]
+        eps = [0.1, 0.01]
+        gbm = CycGBM(kappas=kappas, eps=eps)
+        gbm.train(X, y)
+        z_hat = gbm.predict(X)
+
+        loss = gbm.dist.loss(z_hat, y).sum()
+
+        self.assertAlmostEqual(
+            first=loss,
+            second=expected_loss,
+            places=5,
+            msg="CycGBM Normal distribution loss not as expected",
         )
