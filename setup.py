e9a6abbdcbef751909d7f0848b21be4a0b4aaf32
henningzakrisson <henning.zakrisson@gmail.com>
2023-07-07 14:45:10 +0200

    change version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="cyc-gbm",
-    version="0.0.25",
+    version="0.0.3",
     author="Henning Zakrisson",
     author_email="henning.zakrisson@gmail.com",
     description="A python package for the Cyclical Gradient Boosting Machine algorithm",
